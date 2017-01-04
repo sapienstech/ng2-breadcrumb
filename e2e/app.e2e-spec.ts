9a3ac2e13dcff23914db4cbd6327534fabf5d462
angular-cli <angular-cli@angular.io>
2017-01-04 09:48:40 +0200

    chore: initial commit from angular-cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { NavigatorPage } from './app.po';
+
+describe('navigator App', function() {
+  let page: NavigatorPage;
+
+  beforeEach(() => {
+    page = new NavigatorPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});

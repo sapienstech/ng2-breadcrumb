34c72c947ec05b27c4dab8f0ebe443ccc24026e5
uriel twito <uriel.t@sapiens.com>
2017-03-30 07:04:51 +0300

    support arrow up

--- src/app/breadcrumb/breadcrumb-popup.component.ts
+++ src/app/breadcrumb/breadcrumb-popup.component.ts
@@ -13,25 +13,16 @@ import {Subscription} from "rxjs/Subscription";
 <div class="popover" >
   <button *ngIf="isShowNextArrow"  #btn3 [ngClass]="{'menu-button':true, 'has-no-popup':!isShowBreadcrumbDropDown,'has-popup':isShowBreadcrumbDropDown,'is-active':showPopup}" (click)="setInitialFilter($event)">
     <i class="fa fa-angle-right menu-button-icon"></i>
-    <!--<svg class="menu-button-icon"-->
-      <!--id="Capa_1" x="0px" y="0px" -->
-  <!--viewBox="0 0 477.175 477.175" -->
-  <!--width="20px" height="30px">-->
-<!--<g>-->
-	<!--<path  class="arrow" d="M360.731,229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1,0s-5.3,13.8,0,19.1l215.5,215.5l-215.5,215.5   c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-4l225.1-225.1C365.931,242.875,365.931,234.275,360.731,229.075z" fill="#FFFFFF"/>-->
-<!--</g>-->
-<!--</svg>-->
-
   </button>
   <div *ngIf="showPopup" class="breadcrumbPopup">
-  <div></div>
+  <div class="arrowUp"></div>
       <h4 *ngIf="breadcrumbDropDown.popupTitle">{{breadcrumbDropDown.popupTitle}}</h4>
       <dcn-search-box class="breadcrumb-popup-search"
                       [searchData]="search"
                       [minLength]="0"
                       (results)="onFiltered($event)">
       </dcn-search-box>
-  
+      
       <div class="breadcrumb-popup-menu">
           <div *ngFor="let nextLink of filteredItems" class="breadcrumb-popup-menu-item">
               <a [routerLink]="[nextLink.url, nextLink.params?nextLink.params:{}]"
